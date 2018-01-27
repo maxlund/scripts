92c7eebaa839758577b9c23a5aa695f995869d0d
Max Lund <maxlun@gmail.com>
2018-01-27 12:04:52 +0100

    working with new display

--- move_windows.py
+++ move_windows.py
@@ -4,7 +4,7 @@ import subprocess
 import time
 
 # script to get around annoying linux mint bug of moving 
-# all open windows to a new display whenever it is connected
+# all open windows to a new display whenever it is connected / extended to
 
 def get(cmd):
     return subprocess.check_output(["/bin/bash", "-c", cmd]).decode("utf-8")
@@ -18,7 +18,7 @@ def un_maximize_windows():
 def shift_windows():
     w_data = [l.split() for l in get("wmctrl -lG").splitlines()]
     for w in w_data:
-        command = "wmctrl -ir " + w[0] + " -e 0,600,600,800,600"
+        command = "wmctrl -ir " + w[0] + " -e 0,2500,650,800,600"
         subprocess.Popen(["/bin/bash", "-c", command])
 
 def maximize_windows():
